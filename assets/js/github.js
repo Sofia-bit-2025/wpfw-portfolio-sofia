6af51acf1d075952e54f238c4217da9adc639ec0
Sofia-bit-2025 <24121681@student.hhs.nl>
2026-09-25 22:18:10 +0200

    git date format toegevoegd

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,3 +1,16 @@
 const GITHUB_API_URL =
   "https://api.github.com/repos/Sofia-bit-2025/wpfw-portfolio-sofia";
-  
+
+  const formatDate = (dateString) => {
+  const date = new Date(dateString);
+
+  if (Number.isNaN(date.getTime())) {
+    return "Datum onbekend";
+  }
+
+  return new Intl.DateTimeFormat("nl-NL", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  }).format(date);
+};
